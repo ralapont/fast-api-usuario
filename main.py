26eef25017dc0343f34bb15ae88293fdc5e5231b
ralapont <rafael.alapont.gomez@gmail.com>
2025-04-13 12:28:34 +0200

    implementa cors

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ import logging
 from app.v1.router.user_router import router as user_router
 from app.v1.router.profesor_router import router as profesor_router
 from app.v1.router.clase_router import router as clase_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.getLogger(__name__)
 
